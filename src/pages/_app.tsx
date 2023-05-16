8a1d294d4d628a063b11289a95848e1fba6cb36d
Seah Ying Xiang <seahyx@gmail.com>
2023-05-17 01:07:41 +0800

    Updated home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
+import Layout from "components/Layout";
 import { type AppType } from "next/dist/shared/lib/utils";
 
 import "npm/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 };
 
 export default MyApp;
